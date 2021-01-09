c86f00e1d5c12e0ab7f68f6891154b76dea9a6ec
Max <max.usoltsev@gmail.com>
2021-01-09 15:27:55 +0400

    cleanup

--- Bot.py
+++ Bot.py
@@ -1,19 +1,16 @@
-import random
-from config import TOKEN
-from config import DB_FILE
-from config import ADMIN_ID
 import logging
-from aiogram import Bot, Dispatcher, executor, types
-from db_conn import DBConnection
-from homeworkbot import HomeworkBot
-from user_repository import UserRepository
-from user import User
-from group import Group
-from grouprepository import GroupRepository
 import uuid
 
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, \
-    InlineKeyboardButton
+from aiogram import Bot, Dispatcher, executor, types
+
+from config import ADMIN_ID
+from config import DB_FILE
+from config import TOKEN
+from db_conn import DBConnection
+from group import Group
+from grouprepository import GroupRepository
+from homeworkbot import HomeworkBot
+from user_repository import UserRepository
 
 # уровень логов
 logging.basicConfig(level=logging.INFO)
@@ -28,8 +25,8 @@ groupRepo = GroupRepository(dbconn)
 
 groups = groupRepo.list_group()
 if groups is None or len(groups) == 0:
-    groupRepo.create_group(Group(str(uuid.uuid4()), "1A класс"))
-    groupRepo.create_group(Group(str(uuid.uuid4()), "2A класс"))
+    for i in range(1, 12, 1):
+        groupRepo.create_group(Group(str(uuid.uuid4()), str(i) + " класс"))
 
 dbconn.close()
 
@@ -55,56 +52,6 @@ async def process_text_message(message: types.Message):
     homeworkbot = HomeworkBot(bot, DB_FILE, ADMIN_ID)
     await homeworkbot.handle_text(message)
     homeworkbot.close()
-
-
-async def inline_kb_teacher_add_group(query: types.CallbackQuery):
-    answer_data = query.data
-    teacher = userRepo.get_teacher()
-    chat_id = query.message.chat.id
-    if answer_data == "teacher:add_group":
-        await bot.send_message(text="Выберите группу, учителем которой Вы являетесь",
-                               reply_markup=make_kb_teacher_add_group(chat_id, teacher))
-
-
-async def process_add_teacher_group(query: types.CallbackQuery):
-    answer_data = query.data
-    teacher = userRepo.get_teacher()
-    group_id = answer_data[len("teacher:add_group"):]
-    userRepo.add_teacher_group(teacher.id, group_id)
-    await bot.send_message(query.message.chat.id, text="Вы успешно стали учителем еще одной группы")
-
-
-async def inline_kb_choose_group_answer(query: types.CallbackQuery):
-    answer_data = query.data
-    chat_id = query.message.chat.id
-    user = userRepo.find_user(chat_id)
-    group_id = user.id
-
-
-async def process_kb_teacher_main_menu(chat_id, teacher):
-    main_teacher_kb = InlineKeyboardMarkup()
-    add_homework_bttn = InlineKeyboardButton(text="Добавить задание", callback_data="teacher:add_homework")
-    add_group_bttn = InlineKeyboardButton(text="Добавить группу", callback_data="teacher:add_group")
-    del_group_bttn = InlineKeyboardButton(text="Удалить группу", callback_data="teacher:del_group")
-
-
-async def make_kb_teacher_add_group(chat_id, teacher, ):
-    groups = groupRepo.list_group()
-    add_groups_kb = InlineKeyboardMarkup()
-    for g in groups:
-        found = False
-        for k in teacher.groups:
-            if g == k:
-                found = True
-        button = InlineKeyboardButton(text=g.name, callback_data="teacher:add_group" + g.id)
-        add_groups_kb.add(button)
-        return add_groups_kb
-
-
-async def process_kb_teacher_menu(chat_id, teacher):
-    teacher_main_kb = InlineKeyboardMarkup()
-    for i in teacher.groups:
-        teahcergroups_button = InlineKeyboardButton(text=i.name)
 
 
 # Запуск лонг поллинга
--- db_conn.py
+++ db_conn.py
@@ -7,7 +7,6 @@ class DBConnection:
         self.connection = None
         try:
             self.connection = sqlite3.connect(path)
-            print("Connection to SQLite DB successful")
         except Error as e:
             print(f"The error '{e}' occurred")
 
